67cad6e1d689721ff2d0968deb64768cb056935f
LE CORRE leo <karmaessence@outlook.fr>
2021-11-25 17:46:42 +0100

    save-3

--- polish.ml
+++ polish.ml
@@ -1,5 +1,3 @@
-
-
 (** Projet Polish -- Analyse statique d'un mini-langage impératif *)
 
 (** Note : cet embryon de projet est pour l'instant en un seul fichier
@@ -49,11 +47,17 @@ and block = (position * instr) list
 type program = block
 
 type file_line = { number : position ; indentation : int; content : string}
-(***********************************************************************)
-(*A mettre dans utils.ml*)
-
-(*Permet de savoir le si le mot est un opérateur*)
-let is_operator word = 
+ 
+ let is_comp word = 
+ match word with
+   | "<=" -> true
+   | "<"  -> true
+   | ">=" -> true
+   | ">"  -> true
+   | "="  -> true
+   | _  -> true
+ 
+ let is_operator word = 
   match word with
     | "+" -> true
     | "-"  -> true
@@ -61,8 +65,9 @@ let is_operator word =
     | "/" -> true
     | "%" -> true
     | _ -> false  
-
-(*Permet de savoir le si le mot est un nombre*)
+  ;;
+  
+  (*Permet de savoir le si le mot est un nombre*)
 let is_number word =  
   match word with
     | "0" -> true
@@ -76,84 +81,42 @@ let is_number word =
     | "8" -> true
     | "9" -> true
     | _ -> false  
-
-(*Permet de savoir le si le mot est un operateur de comparaison*)   
-let is_comp word = 
-  match word with
-    | "<=" -> true
-    | "<"  -> true
-    | ">=" -> true
-    | ">"  -> true
-    | "="  -> true
-    | "<>"  -> true  
-    | _ -> false  
-
-(*Transforme le mot en operateur*)
-let get_operator word = 
+    ;;
+  
+  let get_operator word = 
   match word with
     | "+" -> Add
     | "-"  -> Sub
     | "*" -> Mul
     | "/" -> Div
-    | _ -> Mod  
-
-(*Transforme le mot en operateur de comparaison*)
-let get_condition word =
-  match word with
-    | "<=" -> Le
-    | "<"  -> Lt
-    | ">=" -> Ge
-    | ">"  -> Gt
-    | "="  -> Eq
-    | _  -> Ne
-   
-
-(*Calcul le nombre d'espace avant le début de la ligne*)    
-let rec get_space_num line iteration number_of_space =
-  if iteration < String.length line then
-     if String.get line iteration = ' ' then
-       get_space_num line (iteration + 1) (number_of_space + 1)
-     else
-       get_space_num line (iteration + 1) number_of_space
-  else
-     number_of_space    
-
-(*Retourne une liste sans les premiers elements (de i à j non compris)*)
-let rec list_without_first_elements list current_iteration min_iteration  = 
-match list with
-| [] -> []
-| element::sublist ->
-  if current_iteration < min_iteration then
-    list_without_first_elements sublist (current_iteration+1) min_iteration
-  else
-    let resultList = list_without_first_elements sublist (current_iteration+1) min_iteration in
-    element :: resultList
-
-
-(*Retourne l'indentation de la ligne*)
-let rec get_indentation_from_line line count =
-  let line_size = String.length line in
-  if line_size = 0 then
-    count
-    else
-      if String.get line 0 = ' ' then
-        get_indentation_from_line (String.sub line 1 (line_size-1)) (count + 1)
-      else
-        count  
-
-(*transforme le string en tableau de string1*)
-let rec word_to_char_list word index list =
-  if String.length word = List.length list then
-    list
-  else
-    let make_string_from_char = String.make 1 (String.get word index) in
-    word_to_char_list word (index + 1) 
-    ((make_string_from_char)::list) ;;     
+    | _ -> Mod   
+    ;;
     
+    let get_condition word =
+      match word with
+       | "<=" -> Le
+       | "<"  -> Lt
+       | ">=" -> Ge
+       | ">"  -> Gt
+       | "="  -> Eq
+       | _  -> Ne
+    ;;
+    
+    let rec get_indentation_from_line line count =
+      let line_size = String.length line in
+      if line_size = 0 then
+          count
+      else
+          if String.get line 0 = ' ' then
+            get_indentation_from_line (String.sub line 1 (line_size-1)) (count + 1)
+          else
+            count
+;;     
 
-(***********************************************************************)    
-
-(*Construit une instruction avec une liste de mot sans le mot clé*)
+let indentation_from_line = get_indentation_from_line "  Leo" 0;;
+    
+    
+    (*Construit une instruction avec une liste de mot sans le mot clé*)
 let rec construct_expression list_of_words = 
   let first_word = List.nth list_of_words 0 in
   if is_operator first_word then
@@ -168,14 +131,56 @@ let rec construct_expression list_of_words =
 
   else  
     Var (first_word)
+    ;;
+    
+     let rec list_without_first_elements list current_iteration min_iteration  = 
+  match list with
+  | [] -> []
+  | element::sublist ->
+    if current_iteration < min_iteration then
+      list_without_first_elements sublist (current_iteration+1) min_iteration
+    else
+      let resultList = list_without_first_elements sublist (current_iteration+1) min_iteration in
+      element :: resultList
+      ;;
+      
 
+let rec word_to_char_list word index list =
+  if String.length word = List.length list then
+    list
+  else
+    let make_string_from_char = String.make 1 (String.get word index) in
+    word_to_char_list word (index + 1) 
+    ((make_string_from_char)::list) ;;
+
+word_to_char_list "1+1" 0 [];;
+String.make 1 'c';;
+String.split_on_char ' ' "1+1";; 
+      
 let make_condition list_of_words =
-  let word_to_list_1 = word_to_char_list (List.nth list_of_words 0) 0 [] in
-  let word_to_list_2 = word_to_char_list (List.nth list_of_words 2) 0 [] in
-  let exp_1 = construct_expression word_to_list_1 in 
-  let exp_2 = construct_expression word_to_list_2 in 
-  (exp_1, get_condition (List.nth list_of_words 1), exp_2)    
-       
+    let word_to_list_1 = word_to_char_list (List.nth list_of_words 0) 0 [] in
+    let word_to_list_2 = word_to_char_list (List.nth list_of_words 2) 0 [] in
+    let exp_1 = construct_expression word_to_list_1 in 
+    let exp_2 = construct_expression word_to_list_2 in 
+    (exp_1, get_condition (List.nth list_of_words 1), exp_2);;
+make_condition  ["1+1"; "<="; "2"]    
+
+let rec list_without_first list iteration = 
+  match list with
+  | [] -> []
+  | element::sublist ->
+    if iteration = 0 then
+      list_without_first sublist 1
+    else
+      let resultList = list_without_first sublist (iteration+1) in
+      element :: resultList
+      ;;
+      
+list_without_first_elements [1;2;3] 0 1;;
+
+
+      
+      
 (*Explore la liste des lignes et renvoie une liste de block*)
 let rec convert_string_to_block list_of_lines indentation = 
   match list_of_lines with 
@@ -216,8 +221,7 @@ let rec convert_string_to_block list_of_lines indentation =
               else
                 obtain_else_block sub_list_of_lines
           in
-          
-          (*Prend une liste de mot et renvoie une instruction*)
+                    (*Prend une liste de mot et renvoie une instruction*)
           let make_instruction list_of_lines indentation list_of_words first_word = 
             match first_word with 
             | "READ" -> Read (List.nth list_of_words 0)
@@ -240,6 +244,130 @@ let rec convert_string_to_block list_of_lines indentation =
           let sub_list_of_words = list_without_first_elements list_of_words 0 1 in
           (element.number, make_instruction sub_list_of_lines (indentation + 2) 
           sub_list_of_words first_word)::list_result
+        ;;
+
+(***********************************************************************)
+
+   let make_string_operator op = 
+     match op with
+       | Add -> "+"
+       | Sub -> "-"
+       | Mul -> "*"
+       | Div -> "/"
+       | Mod -> "%"
+       ;;
+       
+    let make_string_comparation comp = 
+      match comp with
+        | Eq -> " = "
+        | Ne -> " <> "
+        | Lt -> " < "
+        | Le -> " <= "
+        | Gt -> " > "
+        | Ge -> " >= "
+   
+   
+   
+   let rec make_string_expression expr = 
+     match expr with
+       | Num (value) -> string_of_int value
+       | Var (name) -> name
+       | Op (op, expr1, expr2) ->
+         let string_1 = make_string_expression expr1 in
+         let string_2 = make_string_expression expr2 in
+         let string_op = make_string_operator op in
+         string_op ^ " " ^ string_1 ^ " " ^ string_2
+       ;;
+      
+make_string_expression (Op (Add, Num 1, Num 2));;
+
+  let make_string_condition cond = 
+     match cond with
+     | (expr1, comp, expr2) -> 
+        let string_expr1 = make_string_expression expr1 in
+        let string_expr2 = make_string_expression expr2 in
+        let string_comp = make_string_comparation comp in 
+        string_expr1 ^ string_comp ^ string_expr2
+  ;;
+        
+   let rec convert_block_to_string list_of_block indentation =
+     match list_of_block with
+       | [] -> []
+       | (position, instruction)::sub_list_of_block ->
+      
+          let make_string_instruction position instruction list_file_lines indentation = 
+            match instruction with
+              | Set (name, expr) ->
+                let contents = name ^ " := " ^ make_string_expression expr in
+                let line = { number = position; indentation = indentation; content = contents} in
+                [line] @ list_file_lines
+              | Read (name) ->
+                let contents = "READ " ^ name in
+                let line = { number = position; indentation = indentation; content = contents} in
+                [line] @ list_file_lines
+              | Print (expr) -> 
+                let contents = "PRINT " ^ make_string_expression expr in
+                let line = { number = position; indentation = indentation; content = contents} in
+                [line] @ list_file_lines
+              | If (cond, block_if, block_else) ->
+                let contents = "IF " ^ make_string_condition cond in
+                let line = { number = position; indentation = indentation; content = contents} in
+                (*let lines = [line] @ list_file_lines in *)
+                let block_lines_if = convert_block_to_string block_if (indentation + 2) in
+                
+                if List.length block_else > 0 then
+                  
+                  let block_lines_else = convert_block_to_string block_else (indentation + 2) in
+                  let line_with_if_block = [line] @ block_lines_if  in
+                  let else_pos = (List.length block_lines_if) + position + 1 in 
+                  let line_else = { number = else_pos; indentation = indentation; content = "ELSE"} in
+                  let lines_with_else =  line_with_if_block @ [line_else] in
+                  let line_else_content = lines_with_else @ block_lines_else  in
+                  (*block_lines_else @ lines_with_else*)
+                   list_file_lines @ line_else_content
+                  
+                else
+                  [line] @ block_lines_if
+                  
+              | While (cond, block) ->
+                let contents = "WHILE " ^ make_string_condition cond in
+                let line = { number = position; indentation = indentation; content = contents} in
+                let lines = [line] @ list_file_lines in 
+                let block_lines = convert_block_to_string block (indentation + 2) in
+                lines @ block_lines
+
+          in 
+          let list_file_lines_all = convert_block_to_string sub_list_of_block indentation in
+          let list_file_lines = make_string_instruction position instruction [] indentation in
+          
+          list_file_lines @ list_file_lines_all
+          
+        ;;
+
+
+(***********************************************************************)   
+
+let rec make_space indentation result = 
+  if indentation <= 0 then
+    result
+  else
+    make_space (indentation - 1) (" " ^ result);;
+
+let rec print_lines file_lines =
+  match file_lines with 
+    | [] -> print_string ""
+    | element::sub_file_lines ->
+      let pos = string_of_int element.number in
+      let indentation =  make_space element.indentation "" in
+      print_string (pos ^ ". " ^ indentation ^ element.content ^ "\n");
+      print_lines sub_file_lines;;
+
+let print_polish (p:program) : unit = 
+  let file_lines = convert_block_to_string p 0 in
+  print_lines file_lines;;
+
+
+(***********************************************************************)  
 
 (*Lis le fichier et récupère toute les lignes renvoie un type file_line*)
 let rec get_lines_from_files file iteration get_lines =
@@ -249,138 +377,32 @@ let rec get_lines_from_files file iteration get_lines =
     (*print_endline current_line;*) (*vérifons qu'il existe des espaces*)
     (*Format.printf "%d\n" current_indentation;*) (*comptons le nombre d'espaces*)
     get_lines_from_files file (iteration + 1) ({number = iteration; indentation = current_indentation; content = current_line}::get_lines)
-  with End_of_file -> get_lines   
+  with End_of_file -> get_lines ;; 
 
-(**)
 let read_polish (filename:string) : program = 
   try
     let file = open_in filename in
     let list_of_lines = List.rev(get_lines_from_files file 0 []) in (*renvoie ce qu'il faut*)
+    print_lines list_of_lines;
     convert_string_to_block list_of_lines 0
   with Sys_error _ -> 
     let () = print_endline ("Cannot read filename : " ^ filename) in
-    [] 
+    [] ;;
 
 
 
-let eval_polish (p:program) : unit = failwith "TODO"
-
-let makeListOfLine (filename:string) : unit =
-  try
-    let file = open_in filename in 
-    let list_of_content_in_file = List.rev(get_lines_from_files file 0 [])
-    (*in print_string (List.nth list_of_content_in_file 0)*)
-    in print_string (List.nth list_of_content_in_file 0).content
-  with Sys_error _ -> print_endline ("Cannot read filename : " ^ filename)
-
-(***********************************************************************)
-
-let make_string_operator op = 
-  match op with
-    | Add -> "+"
-    | Sub -> "-"
-    | Mul -> "*"
-    | Div -> "/"
-    | Mod -> "%"
-    ;;
-    
- let make_string_comparation comp = 
-   match comp with
-     | Eq -> " = "
-     | Ne -> " <> "
-     | Lt -> " < "
-     | Le -> " <= "
-     | Gt -> " > "
-     | Ge -> " >= "
-
-
-
-let rec make_string_expression expr = 
-  match expr with
-    | Num (int) -> string_of_int 1
-    | Var (name) -> name
-    | Op (op, expr1, expr2) ->
-      let string_1 = make_string_expression expr1 in
-      let string_2 = make_string_expression expr2 in
-      let string_op = make_string_operator op in
-      string_op ^ " " ^ string_1 ^ " " ^ string_2
-    ;;
-   
-make_string_expression (Op (Add, Num 1, Num 2));;
-
-let make_string_condition cond = 
-  match cond with
-  | (expr1, comp, expr2) -> 
-     let string_expr1 = make_string_expression expr1 in
-     let string_expr2 = make_string_expression expr2 in
-     let string_comp = make_string_comparation comp in 
-     string_expr1 ^ string_comp ^ string_expr2
-
-let rec convert_block_to_string list_of_block indentation =
-  match list_of_block with
-    | [] -> []
-    | (position, instruction)::sub_list_of_block ->
-       
-      let make_string_instruction position instruction list_file_lines indentation = 
-        match instruction with
-          | Set (name, expr) ->
-            let contents = name ^ " := " ^ make_string_expression expr in
-            let line = { number = position; indentation = indentation; content = contents} in
-            [line] @ list_file_lines
-          | Read (name) ->
-            let contents = "READ " ^ name in
-            let line = { number = position; indentation = indentation; content = contents} in
-            [line] @ list_file_lines
-          | Print (expr) -> 
-            let contents = "PRINT " ^ make_string_expression expr in
-            let line = { number = position; indentation = indentation; content = contents} in
-            [line] @ list_file_lines
-          | If (cond, block_if, block_else) ->
-            let contents = "IF " ^ make_string_condition cond in
-            let line = { number = position; indentation = indentation; content = contents} in
-            (*let lines = [line] @ list_file_lines in *)
-            let block_lines_if = convert_block_to_string block_if (indentation + 2) in
-                 
-            if List.length block_else > 0 then
-              let block_lines_else = convert_block_to_string block_else (indentation + 2) in
-              let line_with_if_block = [line] @ block_lines_if  in
-              let else_pos = (List.length block_lines_if) + position + 1 in 
-              let line_else = { number = else_pos; indentation = indentation; content = "ELSE"} in
-              let lines_with_else =  line_with_if_block @ [line_else] in
-              let line_else_content = lines_with_else @ block_lines_else  in
-              list_file_lines @ line_else_content
-            
-            else
-              [line] @ block_lines_if
-                   
-          | While (cond, block) ->
-            let contents = "WHILE " ^ make_string_condition cond in
-            let line = { number = position; indentation = indentation; content = contents} in
-            let lines = [line] @ list_file_lines in 
-            let block_lines = convert_block_to_string block (indentation + 2) in
-            lines @ block_lines
- 
-        in 
-        let list_file_lines_all = convert_block_to_string sub_list_of_block indentation in
-        let list_file_lines = make_string_instruction position instruction [] indentation in
-           
-        list_file_lines @ list_file_lines_all     
-
-let print_polish (p:program) : unit = failwith "TODO"
-
-
-(***********************************************************************)  
+let eval_polish (p:program) : unit = failwith "TODO";;
 
 let usage () =
   print_string "Polish : analyse statique d'un mini-langage\n";
-  print_string "usage: à documenter (TODO)\n"
+  print_string "usage: à documenter (TODO)\n";;
 
 let main () =
   match Sys.argv with
   | [|_;"--reprint";file|] -> print_polish (read_polish (String.trim file))
   | [|_;"--eval";file|] -> eval_polish (read_polish (String.trim file))
-  | [|_;"--test";file|] -> makeListOfLine (String.trim file)
-  | _ -> usage ()
+  | [|_;"--test";file|] -> print_polish ([(1, Set ("res", Op (Sub, Num 0, Var "n"))); (2, While ((Var "n", Lt, Num 2), [(3, Print (Var "n"))])); (4, Read "n")])
+  | _ -> usage ();;
 
 (* lancement de ce main *)
-let () = main ()
+let () = main ();;
