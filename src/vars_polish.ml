af91cc4a6e1a2e5db79d33cd48d9fa6dafc06b11
Akibo WorldBorder <kevin.quach360@gmail.com>
2021-12-22 19:26:15 +0100

    update-16

--- src/vars_polish.ml
+++ src/vars_polish.ml
@@ -9,11 +9,31 @@ open Utility_pf5
 (*                             vars_polish                             *)
 (***********************************************************************)
 
-let print_element key value =
+let rec vars_expr expr map = 
+  match expr with
+  | Num (value) -> 
+    map
+  | Var (name) -> 
+    if NameTable.mem name map then
+      map
+    else
+      NameTable.add name (name ^ " ") map  
+
+  | Op (op, expr_1, expr_2) ->
+    let new_map = vars_expr expr_1 map in
+    vars_expr expr_2 new_map
+
+let print_first_line key value =
   print_string (key ^ " ")
 
-let print_map map =
-    NameTable.iter print_element map
+let print_second_line key value =
+  print_string value
+
+let find_map map =
+    NameTable.iter print_first_line map
+
+let find_map2 map =
+  NameTable.iter print_second_line map
 
 let rec vars_block list_of_block map = 
   match list_of_block with
@@ -22,11 +42,16 @@ let rec vars_block list_of_block map =
 
     match instruction with
     | Set (name, expr) ->
-      let new_map = NameTable.add name name map in
-      vars_block sub_list_of_block new_map
+      if NameTable.mem name map then
+        let new_map = vars_expr expr map in 
+        vars_block sub_list_of_block new_map
+      else
+      let new_map = NameTable.add name "" map in
+      let new_map2 = vars_expr expr new_map in
+      vars_block sub_list_of_block new_map2
 
     | Read (name) ->
-      let new_map = NameTable.add name name map in
+      let new_map = NameTable.add name "" map in
       vars_block sub_list_of_block new_map
 
     | Print (expr) -> 
@@ -46,4 +71,7 @@ let vars_polish (p:program) : unit =
   let map = NameTable.empty in 
   print_string "\n"; 
   let res = vars_block p map in
-  print_map res 
+  find_map res;
+  print_string "\n"; 
+  find_map2 res
+
